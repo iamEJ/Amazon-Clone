5b388ade02ff7f029cb7efe4a16c584279c2ba66
iamEJ <jukneviciute185@gmail.com>
2020-10-05 20:28:54 +0300

    Build the navigation bar

---
+++ src/Components/Header.js
@@ -0,0 +1,53 @@
+import React from "react";
+import "./CSS/Header.css";
+import { Link } from "react-router-dom";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+
+function Header() {
+  return (
+    <nav className="header">
+      <Link to="/">
+        <img
+          className="header__logo"
+          alt=""
+          src="https://onlinebusinessmanager.com/wp-content/uploads/2018/09/white-amazon-logo-png-6.png"
+        />
+      </Link>
+      <div className="header__search">
+        <input type="text" className="header__searchInput" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+      <div className="header__nav">
+        <Link to="/login" className="header__link">
+          <div className="header__navOption">
+            <span className="header__navOptionLineOne">Hello, Jonny</span>
+            <span className="header__navOptionLineTwo">Sign In</span>
+          </div>
+        </Link>
+        <Link to="/" className="header__link">
+          <div className="header__navOption">
+            <span className="header__navOptionLineOne">Returns</span>
+            <span className="header__navOptionLineTwo">& Orders</span>
+          </div>
+        </Link>
+        <Link to="/" className="header__link">
+          <div className="header__navOption">
+            <span className="header__navOptionLineOne">Your</span>
+            <span className="header__navOptionLineTwo">Prime</span>
+          </div>
+        </Link>
+        <Link to="/checkout" className="header__link">
+          <div className="header_optionBasket">
+            <ShoppingBasketIcon />
+            <span className="header__navOptionLineTwo header_basketCount">
+              0
+            </span>
+          </div>
+        </Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
