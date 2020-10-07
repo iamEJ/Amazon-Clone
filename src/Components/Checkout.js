9731acf30ba9312654df4e0905814dbb06f0dcd0
iamEJ <jukneviciute185@gmail.com>
2020-10-07 15:35:58 +0300

    add, style checkout component, add 'remove form basket' funcionality

---
+++ src/Components/Checkout.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { useStateValue } from "./StateProvider";
+import "./CSS/Checkout.css";
+import CheckoutProduct from "./CheckoutProduct";
+
+function Checkout() {
+  const [{ basket }] = useStateValue();
+  return (
+    <div className="checkout">
+      <div className="ckeckout__left">
+        <img
+          className="checkout__ad"
+          alt=""
+          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+        />
+        {basket?.length === 0 ? (
+          <div>
+            <h2>Your Shopping Basket is empty</h2>
+            <p>
+              You have no items in your basket. To buy one or more items, click
+              "Add to Basket" next to the item.
+            </p>
+          </div>
+        ) : (
+          <div>
+            <h2 className="checkout__title">Your Shopping Basket</h2>
+            {/* List of the product in the basket */}
+            {basket.map((item) => (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+            ))}
+          </div>
+        )}
+      </div>
+      {basket.length > 0 && (
+        <div className="checkout__right">
+          <h1>Subtotal</h1>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Checkout;
--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,14 @@
 export const initialState = {
-  basket: [],
+  basket: [
+    {
+      id: "11234",
+      title:
+        "Love Real Food: More Than 100 Feel-Good Vegetarian Favorites to Delight the Senses and Nourish the Body: A Cookbook",
+      price: 12.55,
+      rating: 5,
+      image: "https://images-na.ssl-images-amazon.com/images/I/A16VN49ZV1L.jpg",
+    },
+  ],
   user: null,
 };
 
@@ -10,9 +19,27 @@ function reducer(state, action) {
       //logic for adding item to basket
       return { ...state, basket: [...state.basket, action.item] };
       break;
-    case "REMOVE_TO_BASKET":
+    case "REMOVE_FROM_BASKET":
       // logic to remove
-      return { state };
+
+      //we clone the basket
+      let newBasket = [...state.basket];
+
+      // check if item exist
+      const index = state.basket.findIndex(
+        (basketItem) => basketItem.id === action.id
+      );
+
+      if (index >= 0) {
+        // item exist remove it
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(
+          `Can't remove product (id ${action.id}) as it is not in the basket`
+        );
+      }
+
+      return { ...state, basket: newBasket };
       break;
     default:
       return state;
