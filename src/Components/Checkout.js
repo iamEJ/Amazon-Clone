e6d1764859705a959378948610af7bbb0044a91d
iamEJ <jukneviciute185@gmail.com>
2020-10-07 16:54:52 +0300

    add subtotal component

--- src/Components/Checkout.js
+++ src/Components/Checkout.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useStateValue } from "./StateProvider";
 import "./CSS/Checkout.css";
 import CheckoutProduct from "./CheckoutProduct";
+import Subtotal from "./Subtotal";
 
 function Checkout() {
   const [{ basket }] = useStateValue();
@@ -39,7 +40,7 @@ function Checkout() {
       </div>
       {basket.length > 0 && (
         <div className="checkout__right">
-          <h1>Subtotal</h1>
+          <Subtotal />
         </div>
       )}
     </div>
