19bb6ab659a4fec89057e30bd8705849515ddb2c
iamEJ <jukneviciute185@gmail.com>
2020-10-08 13:12:13 +0300

    style login page

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,9 +1,42 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./CSS/Login.css";
 
 function Login() {
+  const login = (e) => {
+    e.preventDefault(); //stops the refresh
+  };
+  const register = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="login">
+      <Link to="/">
+        <img
+          className="login__image"
+          alt=""
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1280px-Amazon_logo.svg.png"
+        />
+      </Link>
+      <div className="login__container">
+        <h1>Sign-In</h1>
+        <form>
+          <h5>E-mail</h5>
+          <input type="email" />
+          <h5>Password</h5>
+          <input type="password" />
+          <button type="submit" onClick={login} className="login__signInButton">
+            Sign In
+          </button>
+        </form>
+        <p>
+          By continuing, you agree to Amazon's Conditions of Use and Privacy
+          Notice.
+        </p>
+        <button onClick={register} className="login__amazonAccountButton">
+          Create your Amazon Account
+        </button>
+      </div>
     </div>
   );
 }
